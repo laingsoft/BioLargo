1ac89ce2d5f95318b34eda7c33a52611392c26f4
Charles Laing <cclaing@ualberta.ca>
2017-09-25 15:33:53 -0600

    Styling. Added graphs to experiment page

--- app/static/app/js/experiment.js
+++ app/static/app/js/experiment.js
@@ -3,7 +3,7 @@
 
 function makeTable(jsondata){
     table = jQuery("#experimental-data"), row = null, data = null;
-    console.log(jsondata);
+    //console.log(jsondata);
     makeHeaders(jsondata, table);
     $.each(jsondata, function(key, obj){
         row = $('<tr></tr>'); // Create a new row
@@ -18,18 +18,44 @@ function makeHeaders(jsondata, table){
     console.log(jsondata);
     row = $('<tr></tr>');
     for (prop in jsondata[0]){
-        console.log(prop);
+        //console.log(prop);
         $('<th></th>',{text:prop}).appendTo(row);
     }
     row.appendTo(table);
 
 
 }
+
+function makeChart(jsondata){
+    var time = [];
+    var removal = [];
+    var test = [1,2,3,4,5,6,7,8,9,5,4,2,7,8];
+    $.each(jsondata, function(key, obj){
+        time.push(obj["Time [min]"]);
+        removal.push(Math.log(obj["StockCFU [CFU/mL]"]) - Math.log(obj["RemainingCFU [CFU/mL]"]));
+    });
+    var ctx = document.getElementById("removalChart");
+    var removalChart = new Chart(ctx, {
+        type: 'line',
+        
+        data: {
+            labels:time,
+            datasets:[
+                {
+                    label: "Stock / Remaining (log10)",
+                    data:removal,
+                }
+            ]
+        }
+        
+    });
+}
 $.ajax({
     url: "/app/experimentjs/"+id,
     dataType: 'json',
     success: function(data) {
-        console.log(data);
+       // console.log(data);
         makeTable(data);
+        makeChart(data);
     }
 });
