b52af0b34256553cd9c8e4fe8fc05a6da98f6354
Kirsten Kwong <kskwong@ualberta.ca>
2017-10-12 15:49:39 -0600

    sorting index works

--- app/static/app/js/index.js
+++ app/static/app/js/index.js
@@ -1,14 +1,34 @@
-var search_by = 'ID';
+var CONTAINER = $('#experiments')
 
-jQuery(document).ready(function($){
-	$(".metadatarow").click(function(){
-	    window.location = $(this).data("href");
-	});
+
+CONTAINER.jsGrid({
+    width: "100%",
+    sorting: true,
+    pageLoading: true,
+    paging: true,
+    autoload: true,
+    //~ data: [{"id": 1, "Chambers": 2,"Diameter":3,"Length":4,"Target":5,"Age (mL)": 5}],
     
-    $(".input-group-btn .dropdown-menu li").click(function(){
-        search_by = $(this).text();
-        $('#search_select').text("Search by " + search_by);
-        });
+    controller: {
+        loadData:   function(filter){
+                var data;
+                
+                request = $.get("/app/experiments_list", filter, function(response){
+                        data = response;
+                    });
+                    
+                return request.promise()
+        }
+    },
+
+    fields: [
+        {name: "id", type:"number", align: "", title: "ID"},
+        {name: "num_chambers", type:"number", align: "", title: "Chambers"},
+        {name: "reactor_diameter", type:"number", align: "", title: "Diameter"},
+        {name: "reactor_length", type:"number", align: "", title: "Length"},
+        {name: "removal_target", type:"text", align: "", title: "Target"},
+        {name: "reactor_age", type:"number", align: "", title: "Age (ml)"},
+        {name: "group__name", type:"test", align: "", title: "Group"},
+        {name: "tags", type:"test", align: "", title: "Tags"},
+    ]
 });
-
-
