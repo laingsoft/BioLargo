8a80012f7f9bca92f59111578caa21350ca9d279
Kirsten Kwong <kskwong@ualberta.ca>
2017-10-26 12:41:37 -0600

    now sorting on index works

--- app/static/app/js/index.js
+++ app/static/app/js/index.js
@@ -10,13 +10,20 @@ function format_filter(filter){
     //~ id is always the first item
     id = filter.id;
     keys.shift();
-    filter_new['id'] = id
+    filter_new['id'] = id;
+    
+    if (filter.hasOwnProperty('sortField')){
+        keys.splice(-2,2)
+        filter_new['sortOrder'] = filter.sortOrder;
+        filter_new['sortField'] = filter.sortField;
+    }
+    
     
     //~ remove pageSize and pageIndex from the end.
-    filter_new['pageSize'] = filter.pageSize
-    filter_new['pageIndex'] = filter.pageIndex
+    filter_new['pageSize'] = filter.pageSize;
+    filter_new['pageIndex'] = filter.pageIndex;
     
-    keys.splice(-2, 2)
+    keys.splice(-2, 2);
     
     if (filter.hasOwnProperty('experiment_data_filters')){
         filter_new['experiment_data_filters'] = filters.experiment_data_filters;
@@ -36,9 +43,7 @@ $.get("/app/get_metadata_template", {}, function(result){
     fields = result.data.map(function(f){
         return {name: f, type: "text", title: f}
         });
-        
-    fields.push({type: "control", itemTemplate: null})
-    
+           
     CONTAINER.jsGrid({
     width: "100%",
     filtering : true,
@@ -51,8 +56,10 @@ $.get("/app/get_metadata_template", {}, function(result){
     
     controller: {
         loadData:   function(filter){
+            console.log(filter)
             var data;
             filter = format_filter(filter)
+            console.log(filter)
             
             request = $.get("/app/experiments_list", filter, function(response){
                     data = response;
