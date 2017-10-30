32cdc3e4f4c2c37481c256818447b0a5af10f797
Charles Laing <cclaing@ualberta.ca>
2017-10-30 13:53:08 -0600

    Created new style

--- app/static/app/js/base.js
+++ app/static/app/js/base.js
@@ -1,10 +1,17 @@
 $(function() {
-
+    var svginjection = document.querySelectorAll('img.iconic-sprite');
+    SVGInjector(svginjection);
+    
     var url = window.location.href;
-
+    
     $(".nav-link").each(function(){
         if (url == (this.href)){
             $(this).addClass("active");
         }
     });
 });
+
+function doneLoad(){
+    $("#loader").hide();
+    $("#content").slideDown();
+}
