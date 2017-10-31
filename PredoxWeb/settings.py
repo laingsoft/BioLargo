f2c991dc457ca536fcff3837d2ea5112cbf038e0
Kirsten Kwong <kskwong@ualberta.ca>
2017-10-31 09:51:45 -0600

    reorganizing files

--- PredoxWeb/settings.py
+++ PredoxWeb/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'PredoxWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'admin-theme/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
