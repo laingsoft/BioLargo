8e58e4d379c5cd648a7c09e853f0368e1b86570e
Charles Laing <cclaing@ualberta.ca>
2017-12-12 11:26:34 -0700

    Pushing management stuff.

--- PredoxWeb/urls.py
+++ PredoxWeb/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     url(r'^api/',include('api.urls')),
     url(r'api-token-auth/', obtain_jwt_token),
     url(r'api-token-refresh/', refresh_jwt_token),
+    url(r'management/',include('management.urls'))
 ]
--- management/models.py
+++ management/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from accounts.models import Company
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
+
+class Settings(models.Model):
+    Company = models.ForeignKey(Company)
+    dateformat = models.CharField(max_length=10)
+    ataglance = JSONField() #May change in the future
+    
