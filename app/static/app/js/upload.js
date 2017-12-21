97d988ff929b16b38d4077f54634b98f5cb00f03
Kirsten Kwong <kskwong@ualberta.ca>
2017-12-21 15:10:12 -0700

    changing templates works. validationon table does not work

--- app/static/app/js/upload.js
+++ app/static/app/js/upload.js
@@ -1,3 +1,14 @@
+var container = document.getElementById('experiment-data');
+var col_headers;
+var col_validators;
+var hot;
+var options = {data: [['']],
+    rowHeaders: true,
+    contextMenu: true,
+    preventOverflow: 'horizontal',
+    manualRowMove: true,
+}
+
 $("document").ready(function(){
 
     form = $("input[type=radio]:checked").val()
@@ -6,6 +17,80 @@ $("document").ready(function(){
     }
 
 });
+
+
+// will use default validator for dates?
+var VALIDATORS = {
+    "INT" : /\d*/,
+    "FLOAT" : /\d*(\.\d+)?/,
+    "STRING" : /.*/
+};
+
+$('#add-row').click(function() {
+    hot.alter('insert_row', 1);
+});
+
+$('#template-select').change(function() {
+    get_template($(this).val());
+});
+
+$("#exp-form").submit(function(e) {
+
+    $("#template-select").attr("disabled")
+    headers = hot.getColHeader();
+    data = hot.getData();
+
+    metadata = document.getElementById("metadata-fields");
+    metadata = metadata.getElementsByTagName("input");
+
+    metadata_values = {}
+
+    for (var i = 0 ; i < metadata.length; i++){
+        key = metadata[i].labels[0].textContent
+        metadata_values[key] = metadata[i].value
+    }
+
+    console.log(metadata_values)
+
+    var parsed = [];
+    for (row = 0; row < data.length; row++) {
+        d = {};
+        for (h = 0; h < headers.length; h++) {
+            d[headers[h]] = data[row][h];
+        }
+        parsed.push(d);
+    }
+    console.log(parsed)
+
+    $("#id_data-json").val(JSON.stringify({'metadata': metadata_values, 'data': parsed}));
+});
+
+function update_metadata(fields){
+    var container = $("#metadata-fields")
+    container.empty()
+    for (i=0; i < fields.length; i++){
+        container.append("<label for='id_"+ fields[i][0] +"'>"+ fields[i][0] + "</label><input id='id_" + fields[i][0] + "' name='"+ fields[i][0] + "' class='form-control'></input>")
+    }
+}
+
+function update_data_fields(fields){
+    try {
+        hot.destroy() // destroy table
+    }
+    catch(err){
+        // do nothing if the hot instance doesn't exist.
+    }
+
+    col_headers = fields.map(x => x[0]); // set headers.
+    col_validators = fields.map(x => {data:VALIDATORS[x[1]]}); // set validators
+
+    options.colHeaders = col_headers;
+    options.columns = col_validators;
+
+    hot = new Handsontable(container, options);
+
+}
+
 
 $("input[type=radio]").change(function() {
     $("#hidden_fields").append($("#exp_data > .form"))
@@ -40,8 +125,8 @@ var template_select = $('#id_exp_data-template').selectize({
     placeholder: 'Select a template',
     onChange: function(value){
         $.get('/app/get_template', {name: value}, function(result){
-            console.log(result.fields)
-            console.log(result.metadata)
+            update_metadata(result.fields);
+            update_data_fields(result.metadata);
         });
     }
 });
