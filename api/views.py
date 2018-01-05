8ddaf77286d21720ba2000a72ca91a078d5aed43
fadymach <fmachaalani@gmail.com>
2018-01-05 09:58:52 -0700

    Added experiment deletion functionality.

--- api/views.py
+++ api/views.py
@@ -5,8 +5,7 @@ import json
 from app.models import *
 from rest_framework import viewsets
 from .serializers import commentSerializer, tagsSerializer, experimentSerializer, groupSerializer
-
-
+from django.http import Http404
 # Create your views here.
 
 def requestTest(request):
@@ -80,11 +79,20 @@ def get_experiments_id(request):
     data = {k: v.id for k,v in enumerate(data)}
     return JsonResponse(data)
     
-def get_csv():
+def get_csv(request, exp_id):
     pass
 
-def experimentrm():
-    pass
+#This will delete an experiment assuming that the company is correct
+#It requires an Experiment ID to be passed into it. 
+@login_required
+def experimentrm(request, exp_id):
+    company = request.user.company
+    data = Experiment.objects.filter(id = exp_id, company = company)
+    if not data.exists():  
+        raise Http404("Experiment does not exist.")
+    result = data.delete()
+    print("The result is", result)
+    return JsonResponse({"result": result[0]>0})
 
 @login_required
 def comment(request):
