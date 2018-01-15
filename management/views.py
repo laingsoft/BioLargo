c43a4d2c4ad134eee9682052af07b2c761a26c70
Kirsten Kwong <kskwong@ualberta.ca>
2018-01-15 16:08:33 -0700

    added some missing links

--- management/views.py
+++ management/views.py
@@ -25,6 +25,7 @@ class ManagerTestMixin(UserPassesTestMixin):
     class based permissions, add test_func method to view with condition.
     """
     login_url = "/management/login"
+
     def test_func(self):
         return self.request.user.is_authenticated and (self.request.user.is_manager or self.request.user.is_admin)
 
