2532d05da93085477bed28846af9861a448e0ce8
Charles Laing <cclaing@ualberta.ca>
2018-02-01 14:48:40 -0700

    Added dispatcher

---
+++ analytics/dispatcher.py
@@ -0,0 +1,13 @@
+from base_analysis import *
+
+analytics_tools = {
+    "max": max_tool
+    "min": min_tool
+    "avg": simple_avg_tool
+    "nxS": nx_score_tool
+    }
+
+def analytics_socket_dispatch(data, channel):
+    # Socket Accept and response should go here
+    pass
+
