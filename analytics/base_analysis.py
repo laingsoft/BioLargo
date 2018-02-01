e4750dce167ee0e85b5b7403fb773ace91b0eae0
Charles Laing <cclaing@ualberta.ca>
2018-02-01 14:47:57 -0700

    Added n50 score

--- analytics/base_analysis.py
+++ analytics/base_analysis.py
@@ -46,5 +46,32 @@ class simple_avg_tool(Tool):
     def evaluate(self):
         total_sum = sum(self.data)
         return total_sum/len(self.data)
+
+class nx_score_tool(Tool):
+    '''
+    Calculates the N[x] score
+    Accepts x_val, where x_val is the percentage of the entire assembly
+    you want to see
+    '''
+
+    def __init__(self, data, x_val):
+        self.data = data.sort(reverse=True)
+        self.x_val = x_val / 100
+
+    def evaluate(self):
+        total_sum = sum(self.data)
+        i = 0
+        running_sum = 0
+        while running_sum < (total_sum * x_val) :
+            cursor = self.data[i]
+            running_sum += cursor
+            i+=1
+        return cursor
+    
+
+            
+       
+    
+            
     
     
