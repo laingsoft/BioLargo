6eebcdbeb1b822a11f68f3665dbc39055472e8ca
Kirsten Kwong <kskwong@ualberta.ca>
2018-02-13 16:19:56 -0700

    added some stats to project page

--- project_management/templatetags/task_tags.py
+++ project_management/templatetags/task_tags.py
@@ -6,10 +6,38 @@ register = template.Library()
 
 @register.simple_tag
 def incomplete_tasks(user):
+    """
+    gets a user's incomplete tasks
+    """
     return Task.incomplete.filter(assigned=user)
 
 
 @register.simple_tag
 def complete_tasks(user):
+    """
+    gets a user's complete tasks
+    """
     return Task.complete.filter(assigned=user)
 
+
+@register.simple_tag
+def task_progress(project):
+    """
+    calculates progress percentage. complete / total
+    """
+    complete = Task.completed.filter(project=project).count()
+    total = Task.objects.filter(project=project).count()
+    return complete/total * 100
+
+
+@register.simple_tag
+def count_scientists(project):
+    """
+    counts the number of scientists working on a project.
+    """
+    return project.experiment_set.all().distinct('user').count()
+
+
+@register.simple_tag
+def get_tags(project):
+    return project.experiment_set.all().values('tags').distinct('tags')
