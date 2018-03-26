b6010c668b5fac6c403282e64768724c9ce35292
Kirsten Kwong <kskwong@ualberta.ca>
2018-03-26 14:26:59 -0600

    basic drag and drop for tools

--- analytics/static/analysis/js/analysis.js
+++ analytics/static/analysis/js/analysis.js
@@ -1,11 +1,51 @@
+var container = $('#container')
+
+function dragStartHandler(evt) {
+    evt.dataTransfer.effectAllowed = 'move';
+    evt.dataTransfer.setData('text/plain', evt.target.dataset.toolName);
+    return true;
+}
+
+function dragEnterHandler(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+}
+
+function dragOverHandler(evt) {
+    evt.preventDefault();
+}
+
+function dropHandler(evt) {
+    evt.preventDefault();
+    var toolName = evt.dataTransfer.getData('text/plain');
+    container.append(`
+    <div class="row">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-block">
+                    <h5 class="card-title">${toolName}</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+        `)
+
+}
+
 $(document).ready(function(){
     var tools = document.querySelectorAll('.tool-list .list-group-item');
 
-    tools.forEach(function(tool){
-        tool.addEventListener('dragstart', function(evt){
-            evt.stopPropagation();
-            evt.dataTransfer.effectAllowed = 'move';
-            evt.dataTransfer.setData('text/plain', '');
-        });
+    [].forEach.call(tools, function(tool){
+        tool.addEventListener('dragstart', dragStartHandler, false);
     })
+
+    var drop_target = document.querySelector(".main")
+
+
+    drop_target.addEventListener('dragenter', dragEnterHandler)
+    drop_target.addEventListener('dragover', dragOverHandler)
+    drop_target.addEventListener('drop', dropHandler)
+
 })
