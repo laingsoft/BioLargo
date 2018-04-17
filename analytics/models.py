da30f9ea02dbf78c2cba9c83a58e45a0bd9a2f9d
Kirsten Kwong <kskwong@ualberta.ca>
2018-04-17 10:25:44 -0600

    small change to session model and reset migrations

--- analytics/models.py
+++ analytics/models.py
@@ -1,24 +1,19 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.postgres.fields import JSONField
+from project_management.models import Project
 
 
 class Session(models.Model):
     """
-    Stores session metadata.
+    Stores session info and project used in analysis.
     """
     name = models.CharField(max_length=30)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    dataset = JSONField()  # queryset parameters.
+    project = models.ForeignKey(Project)
 
     class Meta:
         unique_together = (('user', 'name'))
-
-    def generate_qs(self):
-        """
-        uses dataset parameters (dictionary) to generate a queryset.
-        """
-        pass
 
 
 class Action(models.Model):
@@ -26,7 +21,7 @@ class Action(models.Model):
     stores action as JSON sent from the client and links to a session.
     action will include what the action was, the settings for the specific
     action (whatever is needed to generated object with js), what is needed to
-    get data set (if using subset of data in session)
+    get data set, etc.
     """
     session = models.ForeignKey(Session)
     action = JSONField()
