572bb98af92dee9845aa700ab3659c072d3c133b
Kirsten Kwong <kskwong@ualberta.ca>
2018-04-17 14:55:05 -0600

    websocket security settings

--- PredoxWeb/routing.py
+++ PredoxWeb/routing.py
@@ -3,13 +3,15 @@ from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
 import app.routing
 import analytics.routing
 from django.conf.urls import url, include
+from channels.security.websocket import AllowedHostsOriginValidator
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket': AuthMiddlewareStack(
+    'websocket': AllowedHostsOriginValidator(
+    AuthMiddlewareStack(
         URLRouter([
             url('app/', include(app.routing.websocket_urlpatterns)),
             url('analytics/', include(analytics.routing.websocket_urlpatterns)),
         ])
-    ),
+    )),
 })
