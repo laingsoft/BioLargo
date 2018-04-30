7c07da042f02d4c567fdda8ef1af0aa19bbc8996
Charles <cclaing@ualberta.ca>
2018-04-30 13:38:52 -0600

    Updated frontpage

--- frontpage/urls.py
+++ frontpage/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'pricing/$', views.pricing),
+    url(r'contactus/$', views.contactus),
+    url(r'support/$', views.support),
+    url(r'features/$', views.features),
     ]
--- frontpage/views.py
+++ frontpage/views.py
@@ -6,3 +6,17 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'frontpage/index.html')
+
+def contactus(request):
+    return render(request, 'frontpage/contactus.html')
+
+def pricing(request):
+    return render(request, 'frontpage/pricing.html')
+
+def support(request):
+    return render(request, 'frontpage/support.html')
+
+def features(request):
+    return render(request, 'frontpage/features.html')
+
+
