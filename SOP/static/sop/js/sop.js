97e71a00164071a70a12a6a7a68f8e5d230a0ced
Charles <cclaing@ualberta.ca>
2018-05-15 16:22:40 -0600

    Added files to not break things

---
+++ SOP/static/sop/js/sop.js
@@ -0,0 +1,33 @@
+
+
+$(document).ready(function(){
+    $(".underlined-hover").click(function(){
+	this.contentEditable = true;
+    });
+
+    $("#submitSOP").click(function(){
+	var csrftoken = getCookie('csrftoken');
+	var name = document.getElementById("sop-name").textContent;
+	var description = document.getElementById("sop-description").textContent;
+	var procedure = JSON.stringify(quill.getContents());
+	var data = {"name":name,
+		    "description": description,
+		    "procedure": procedure
+		   };
+	console.log(data);
+	$.ajax({
+            beforeSend: function(xhr, settings){
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            },
+            url: "/api/sop/"+id,
+            dataType: 'json',
+            method: 'POST',
+            data: data,
+            success: function(data) {
+		console.log("worked");
+
+            }
+        });
+    });
+});
+
