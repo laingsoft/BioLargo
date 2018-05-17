4bc3d9460b4e0b9c5e4be01855c6f3943ea85635
Charles <cclaing@ualberta.ca>
2018-05-17 11:23:29 -0600

    Added search for inventory items

--- inventory/static/inventory/js/inventory.js
+++ inventory/static/inventory/js/inventory.js
@@ -1,3 +1,26 @@
+function searchInventory() {
+    var input, filter, table, rd, td, i;
+    input = document.getElementById("inventory-search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("inventory-table");
+    tr = table.getElementsByTagName("tr");
+    for (i=0; i< tr.length; i++){
+	console.log(tr[i]);
+	td = tr[i].getElementsByTagName("td")[0];
+	if (td){
+	    if (td.innerHTML.toUpperCase().indexOf(filter) > -1){
+		tr[i].style.display = "";
+	    } else {
+		tr [i].style.display = "none";
+	    }
+	}
+    }
+    
+
+}
+
+
+
 $(document).ready(function(){
     $(".inventory-list-item").click(function(){
 	console.log(this)
